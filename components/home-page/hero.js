e61a1dc3de2c92d691b90cb8f046609d9208c552
Maria Campbell <interglobalmedia@gmail.com>
2023-01-16 14:51:33 -0500

    NFB-3 #resolve #time 10m #comment create hero section

---
+++ components/home-page/hero.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import classes from './hero.module.scss'
+
+function Hero() {
+    return (
+        <section className={classes.hero}>
+            <div className={classes.image}>
+                <Image
+                    src="/images/site/profile_image.jpg"
+                    alt="Maria Campbell profile image"
+                    width={300}
+                    height={300}
+                />
+            </div>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            <h1>Hi, I'm Maria!</h1>
+            <p>I blog about fullstack development as well as macOS, Command Line, and Git.</p>
+        </section>
+    )
+}
+
+export default Hero
