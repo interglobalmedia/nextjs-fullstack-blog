ce22419a0630605dd8f47663308ac01391760a85
Maria Campbell <interglobalmedia@gmail.com>
2023-01-17 08:03:42 -0500

    NFB-6 #resolve #time 15m #comment add basic code for component

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,13 @@
+import classes from './featured-posts.module.scss'
+
+function FeaturedPosts() {
+    return (
+        <section className={classes.latest}>
+            <h2>Featured Posts</h2>
+
+            {/* separate component for list of posts here */}
+        </section>
+    )
+}
+
+export default FeaturedPosts
