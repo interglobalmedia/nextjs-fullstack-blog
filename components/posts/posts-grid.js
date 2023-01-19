cc0f6c780c5e3ca89016f2ecbd15de0d25b6e119
Maria Campbell <interglobalmedia@gmail.com>
2023-01-19 09:35:52 -0500

    NFB-7 #resolve #time 1m #comment pass in props to functional component

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -1,11 +1,15 @@
 import PostItem from './post-item'
 import classes from './posts-grid.module.scss'
 
-function PostsGrid() {
+function PostsGrid(props) {
     const { posts } = props;
+
+    console.log(posts)
     return (
         <ul className={classes.grid}>
-        {posts.map(post => <PostItem />)}
+            {posts.map(post => (
+                <PostItem key={post.slug} post={post} />
+            ))}
         </ul>
     )
 }
