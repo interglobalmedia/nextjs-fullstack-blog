cbe02baf2e8699a2954db19a91cdd7b4ed43aac3
Maria Campbell <interglobalmedia@gmail.com>
2023-01-19 19:14:34 -0500

    NFB-9 #resolve #time 15m #comment add post-content and post-header

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,22 @@
+import PostHeader from './post-header'
+import classes from './post-content.module.scss'
+
+const DUMMY_POST = {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with Nextjs',
+    image: 'getting-started-nextjs.png',
+    date: '2023-01-17',
+    content: `# This is a first post`
+}
+
+function PostContent() {
+    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+    return (
+        <article className={classes.content}>
+            <PostHeader title={DUMMY_POST.title} image={imagePath} />
+            {DUMMY_POST.content}
+        </article>
+    )
+}
+
+export default PostContent
