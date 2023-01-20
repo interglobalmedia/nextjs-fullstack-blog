6b53faacca715e9ddbecde8813decf89d1f9ad2c
Maria Campbell <interglobalmedia@gmail.com>
2023-01-20 18:14:56 -0500

    NFB-14 #resolve #time 2m #comment replace dummy content with props

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,20 +2,13 @@ import ReactMarkdown from 'react-markdown'
 import PostHeader from './post-header'
 import classes from './post-content.module.scss'
 
-const DUMMY_POST = {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    date: '2023-01-17',
-    content: `# This is a first post`
-}
-
-function PostContent() {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+function PostContent(props) {
+    const { post } = props
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POST.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     )
 }
