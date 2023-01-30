f84a82a725b46c93bc1aef4556d00212adab31d8
Maria Campbell <interglobalmedia@gmail.com>
2023-01-30 08:10:47 -0500

    NFB-26 #resolve #time 1h #comment add icon component and links

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,10 +1,53 @@
+import Link from 'next/link'
+import siteMetadata from '../../data/siteMetadata'
+
+import SocialIcon from '../../helpers/icons-map'
+import classes from '../../styles/social-icon.module.scss'
+
 function Footer() {
-    const theDate = new Date()
     return (
-        <footer>
-            <p>{`© ${theDate.getFullYear()} Maria D. Campbell`}</p>
+        <footer className={classes['site-footer']}>
+            <div className="mt-16 flex flex-col items-center">
+                <div className="mb-3 flex flex-wrap justify-evenly space-x-4">
+                    <div className={classes.svg}>
+                        <SocialIcon
+                            name="mail"
+                            href={`mailto:${siteMetadata.email}`}
+                            size="6"
+                            className={classes.mail}
+                        />
+                    </div>
+                    <div className={classes.svg}>
+                        <SocialIcon
+                            name="github"
+                            href={siteMetadata.github}
+                            size="6"
+                        />
+                    </div>
+                    <div className={classes.svg}>
+                        <SocialIcon
+                            name="twitter"
+                            href={siteMetadata.twitter}
+                            size="6"
+                        />
+                    </div>
+                    <div className={classes.svg}>
+                        <SocialIcon
+                            name="linkedin"
+                            href={siteMetadata.linkedin}
+                            size="6"
+                        />
+                    </div>
+                </div>
+            </div>
+            <p>
+
+                {`© ${new Date().getFullYear()}`} {` • `}{' '}
+                {siteMetadata.author}{' '} {` • `}
+                <Link href="/">{siteMetadata.title}</Link>
+            </p>
         </footer>
- )
+    )
 }
 
 export default Footer
