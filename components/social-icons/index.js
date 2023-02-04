28a0e43633a90262d28364ca484385fc78cfec48
Maria Campbell <interglobalmedia@gmail.com>
2023-02-04 14:13:30 -0500

    NFB-38 #resolve #time 1m #comment add Sitemap svg icon

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -1,4 +1,5 @@
 export { default as Github } from './Github'
 export { default as Linkedin } from './Linkedin'
 export { default as Mail } from './Mail'
+export { default as Sitemap } from './Sitemap'
 export { default as Twitter } from './Twitter'
