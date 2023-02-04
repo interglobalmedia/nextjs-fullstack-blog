2e183a72a7eb8eb1005c6c80120e8005bbb00cdc
Maria Campbell <interglobalmedia@gmail.com>
2023-02-04 14:19:04 -0500

    NFB-38 #resolve #time 15m #comment add siteMetadata ThemeProvider and next fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.scss'
 import Layout from '../components/layout/layout'
 import Head from 'next/head'
+import siteMetadata from '../data/siteMetadata'
+import { ThemeProvider } from 'next-themes'
 import { motion } from 'framer-motion'
+import { Inconsolata, Roboto, Oswald } from '@next/font/google'
+
+const inconsolata = Inconsolata({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+})
+
+const roboto = Roboto({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+})
+
+const oswald = Oswald({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+})
 
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
@@ -12,31 +30,35 @@ const variants = {
 export default function App({ Component, pageProps, router }) {
 
   return (
-    <Layout>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-        <meta
-          name="author"
-          content="Maria D. Campbell"
-        />
-        <link
-          rel="icon"
-          href="/favicon.ico"
-        />
-      </Head>
-      <motion.div
-        key={router.route}
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        variants={variants}
-        transition={{ duration: 0.5, type: 'tween' }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </Layout>
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta
+            name="author"
+            content="Maria D. Campbell"
+          />
+          <link
+            rel="icon"
+            href="/favicon.ico"
+          />
+        </Head>
+        <motion.div
+          key={router.route}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 0.5, type: 'tween' }}
+        >
+          <main className={`${inconsolata.className}`}>
+            <Component {...pageProps} />
+          </main>
+        </motion.div>
+      </Layout>
+    </ThemeProvider>
   )
 }
