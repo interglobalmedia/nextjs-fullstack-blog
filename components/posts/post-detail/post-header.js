ec318ab924009af161e139dcefd9f53e61764ca8
Maria Campbell <interglobalmedia@gmail.com>
2023-02-10 22:06:48 -0500

    NFB-41 #resolve #time 1m #comment remove image

--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -6,8 +6,6 @@ function PostHeader(props) {
     return (
         <header className={`${classes.header} header`}>
             <h1>{title}</h1>
-            <Image src={image} alt={title} width={200} height={150} />
-
         </header>
     )
 }   
