206b50b04c53bf42775e6ec8ffcd3edb3e1275a1
Maria Campbell <interglobalmedia@gmail.com>
2023-02-11 13:56:41 -0500

    NFB-42 #resolve #time 30m #comment add scroll button components

---
+++ components/buttons/scroll-top.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import { createPortal } from 'react-dom'
+import classes from '../../styles/scroll-top-btn.module.scss'
+
+export default function GoTop() {
+    const [showGoTop, setShowGoTop] = useState(false)
+    const [showMe, setShowMe] = useState(false)
+
+    const handleVisibleButton = () => {
+        if (setShowGoTop(window.pageYOffset > 50)) {
+            setShowGoTop(true)
+            setShowMe(!showMe)
+        } else if (setShowGoTop(window.pageYOffset < 50)) {
+            setShowGoTop(false)
+            setShowMe(showMe)
+        }
+    }
+
+    const handleScrollUp = () => {
+        window.scrollTo({ left: 0, top: 0, behavior: 'smooth' })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleVisibleButton)
+        return () => window.removeEventListener('scroll', handleVisibleButton)
+    }, [])
+
+    if (typeof document === "object") {
+        return createPortal((
+            <>
+                <button className={`${classes.scroll} ${classes.top} ${classes.icon} ${classes['icon-fill']}`} onClick={handleScrollUp} style={{
+                    display: showGoTop ? "none" : "block"
+                }}><span className={`material-icons`}>
+                        keyboard_arrow_up
+                    </span></button>
+            </>
+        ), document.getElementById('scroll-top'))
+    } else {
+        return null
+    }
+}
