f5a9a94bf4fec8b25a578d32d86d46103d6cc791
Maria Campbell <interglobalmedia@gmail.com>
2023-02-13 03:33:00 -0500

    NFB-51 #resolve #time 30m #comment add anonymous function for phase change export config and bundle analyzer config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,29 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-})
-
-const path = require('path')
-
 const webpack = require('webpack')
 
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = withBundleAnalyzer({
-	/* Configure pageExtensions to include md and mdx */
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-	/* Optionally, add any other Next.js config below */
-	reactStrictMode: true,
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+	function(phase) {
+		if (phase === PHASE_DEVELOPMENT_SERVER) {
+			return {
+				env: {
+					mongodb_username: `${MONGODB_USERNAME}`,
+					mongodb_password: `${MONGODB_PASSWORD}`,
+					mongodb_clusername: `${MONGODB_CLUSTERNAME}`,
+					mongodb_database: `${MONGODB_DATABASE_DEV}`
+				}
+			}
+		}
+		return {
+			env: {
+				mongodb_username: `${MONGODB_USERNAME}`,
+				mongodb_password: `${MONGODB_PASSWORD}`,
+				mongodb_clusername: `${MONGODB_CLUSTERNAME}`,
+				mongodb_database: `${MONGODB_DATABASE}`
+			}
+		}
+	},
 
 	webpack: (config, { dev, isServer }) => {
 		config.module.rules.push({
@@ -39,25 +50,14 @@ module.exports = withBundleAnalyzer({
 
 		return config
 	},
-
-	constants: (phase) => {
-		if (phase === PHASE_DEVELOPMENT_SERVER) {
-			return {
-				env: {
-					mongodb_username: `${MONGODB_USERNAME}`,
-					mongodb_password: `${MONGODB_PASSWORD}`,
-					mongodb_clusername: `${MONGODB_CLUSTERNAME}`,
-					mongodb_database: `${MONGODB_DATABASE_DEV}`
-				}
-			}
-		}
-		return {
-			env: {
-				mongodb_username: `${MONGODB_USERNAME}`,
-				mongodb_password: `${MONGODB_PASSWORD}`,
-				mongodb_clusername: `${MONGODB_CLUSTERNAME}`,
-				mongodb_database: `${MONGODB_DATABASE}`
-			}
-		}
-	},
 })
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	/* Configure pageExtensions to include md and mdx */
+	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+	/* Optionally, add any other Next.js config below */
+	reactStrictMode: true,
+}
+
+module.exports = withBundleAnalyzer(nextConfig)
