61251b236ecbd7dfc0a7200efef421d57666d70e
Maria Campbell <interglobalmedia@gmail.com>
2023-02-12 19:25:40 -0500

    NFB-47 #resolve #time 5m #comment use new env vars including dev var

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -18,8 +18,13 @@ async function handler(req, res) {
 
         let client
 
+        const mongodb_username = process.env.MONGODB_USERNAME
+        const mongodb_password = process.env.MONGODB_PASSWORD
+        const mongodb_clustername = process.env.MONGODB_CLUSTERNAME
+        const mongodb_database_dev = process.env.MONGODB_DATABASE_DEV
+
         try {
-            const mongodb_url = process.env.NEXT_PUBLIC_MONGODB_URL
+            const mongodb_url = `mongodb+srv://${mongodb_username}:${mongodb_password}@${mongodb_clustername}.qbavi.mongodb.net/${mongodb_database_dev}`
             client = await MongoClient.connect(`${mongodb_url}`)
 
         } catch (error) {
