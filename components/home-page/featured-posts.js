1b624f3b04d6e8647f5d0e0ca23547e0459c255e
Maria Campbell <interglobalmedia@gmail.com>
2023-02-13 02:03:41 -0500

    NFB-50 #re4solve #time 5m #comment add dynamic imports

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,12 +1,15 @@
-import PostsGrid from '../posts/posts-grid'
+import dynamic from 'next/dynamic'
+// import PostsGrid from '../posts/posts-grid'
 import classes from '../../styles/featured-posts.module.scss'
+
+const DynamicPostsGrid = dynamic(() => import('../posts/posts-grid'))
 
 function FeaturedPosts(props) {
     return (
         <section className={classes.latest}>
             <h2>Featured Posts</h2>
             {/* separate component for list of posts here */}
-            <PostsGrid posts={props.posts} />
+            <DynamicPostsGrid posts={props.posts} />
         </section>
     )
 }
