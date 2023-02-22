5b4f99b29943d59c4afb1e1a3ef2fa21ae18d71a
Maria Campbell <interglobalmedia@gmail.com>
2023-02-22 14:24:24 -0500

    NFB-54 #resolve #time 15m #comment add auth global vars update markup

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,14 +1,26 @@
 import { createPortal } from 'react-dom'
 import Link from 'next/link'
+import { useSession, signIn, signOut } from 'next-auth/react'
 import siteMetadata from '../../data/siteMetadata'
 import SocialIcon from '../../helpers/icons-map'
+import ProviderButtons from '../buttons/provider-buttons'
+import FooterNavigation from './footer-navigation/footer-navigation'
 import classes from '../../styles/footer.module.scss'
 
 function Footer() {
+    const { data: session, status } = useSession()
+
 
     if (typeof document === "object") {
         return createPortal((
-            <footer className={`${classes.footer}`}>
+            <footer className={`footer ${classes.footer}`}>
+                <FooterNavigation />
+                <div className={classes['provider-wrapper']}>
+                    {status === `unauthenticated` && <h2 className={classes['sign-in']}>Sign in</h2>}
+                    {status === `authenticated` && <h2 className={classes['sign-in']}></h2>}
+                    <ProviderButtons />
+                </div>
+                <h2 className={classes.follow}>Follow</h2>
                 <div className={`${classes['svg-wrapper']}`}>
                     <div className={`footer-email ${classes['footer-email']}`}>
                         <SocialIcon
