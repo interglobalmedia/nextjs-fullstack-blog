1e50fb06f6d803232cdc13319027f5ee79f5cb4f
Maria Campbell <interglobalmedia@gmail.com>
2023-02-23 15:47:40 -0500

    NFB-57 #resolve #time 5m #comment update text content

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import classes from '../styles/about.module.scss'
 
 function AboutPage() {
@@ -5,8 +6,9 @@ function AboutPage() {
         <section className={`about-section ${classes['about-section']}`}>
             <h1>About this Site</h1>
             <article className={`about-article ${classes['about-article']}`}>
-                <p className={classes.info}>If you would like to contact me, you can sign in with Github located in the footer of this site and fill out the contact form on the Contact page that appears upon sign in (in progress). Or you can email me directly by clicking on the envelope in the site footer.</p>
-                <p className={classes.info}>When you sign in and fill out the contact form, your message will appear in my Guestbook (in progress) along with your Github, Twitter, and Linkedin handle. Looking forward to hearing from you!</p>
+                <p className={classes.info}>If you would like to contact me, you can sign in with Github located in the footer of this site and fill out the <Link href='/contact'>contact form</Link> on the <Link href='/contact'>Contact page</Link> that appears upon sign in. Or you can email me directly by clicking on the envelope in the site footer.</p>
+                <p className={classes.info}>When you sign in and fill out the contact form, your message will appear in my <Link href='/guestbook'>Guestbook</Link> along with links to your Github, Twitter, and Linkedin handles. Looking forward to hearing from you!</p>
+                <p className={classes.info}>Spam will be removed.</p>
             </article>
         </section>
     )
