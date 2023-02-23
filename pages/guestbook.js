fb31025ef56e265f03579047e58116ce1f98f319
Maria Campbell <interglobalmedia@gmail.com>
2023-02-23 12:15:56 -0500

    NFB-56 #resolve #time 30m #comment add connection to db retrieve data

--- pages/guestbook.js
+++ pages/guestbook.js
@@ -1,9 +1,55 @@
-function GuestBook() {
+import { connectToDB } from '../lib/db'
+import { useSession } from 'next-auth/react'
+import classes from '../styles/guestbook.module.scss'
+function GuestBook({ messages }) {
+    const { data: session, status } = useSession()
+    // const loading = status === "loading"
     return (
-        <div>
-            <h1>Guestbook</h1>
-        </div>
+        <>
+            <section classname={classes['guestbook-section']}>
+                <h1>Guestbook</h1>
+                <ul className={classes['guestbook-list']}>
+                    {messages.map((message) => (
+                        <li key={message._id}>
+                            {message.name}
+                            {console.log(`the user name`, message.name)}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <section className={classes.user}>
+                {
+                    status === `authtenticated` &&
+                    <>
+                        <p style={{ marginBottom: '10px' }}> Welcome, {data.session.user.name && datga.session.user.email}</p> <br />
+                    </>
+                }
+            </section>
+        </>
     )
 }
 
+export async function getServerSideProps() {
+    let messages = []
+    const client = await connectToDB()
+    const db = client.db()
+    const messagesCollection = db.collection('messages')
+    console.log(messagesCollection)
+    const result = await messagesCollection.find({}).toArray()
+    console.log(`the result`, result)
+    const parsedMessages = JSON.parse(JSON.stringify(result))
+    console.log(`the parsed messages`, parsedMessages)
+    client.close()
+    messages = parsedMessages.map((message) => {
+        message.name,
+            message.twitterHandle,
+            message.linkedinHandle,
+            message.githubHandle
+    })
+    return {
+        props: {
+            messages: parsedMessages
+        }
+    }
+}
 export default GuestBook
