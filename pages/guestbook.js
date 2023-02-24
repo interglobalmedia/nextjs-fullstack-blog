84bd8d022e71fdb1b855feac9de7bab7a4c189ec
Maria Campbell <interglobalmedia@gmail.com>
2023-02-23 23:00:11 -0500

    Add back getSession

--- pages/guestbook.js
+++ pages/guestbook.js
@@ -1,55 +1,60 @@
-import { getServerSession } from 'next-auth/next'
-import { useSession } from 'next-auth/react'
+import { getSession } from 'next-auth/react'
 import { connectToDB } from '../lib/db'
 import classes from '../styles/guestbook.module.scss'
 function GuestBook({ messages }) {
-    const { data: session } = useSession()
 
     const twitterUrl = `https://twitter.com/`
     const linkedinUrl = `https://www.linkedin.com/in/`
     const githubUrl = `https://github.com/`
 
-    if (typeof window === "undefined") return null
-    if (session) {
-        return (
-            <>
-                <section className={`guestbook-section ${classes['guestbook-section']}`}>
-                    <h1>Guests</h1>
-                    <ul className={classes['guestbook-list']}>
-                        {messages.map((message) => (
-                            // eslint-disable-next-line react/jsx-key
-                            <li className={`guest ${classes.guest}`}>
+    return (
+        <>
+            <section className={`guestbook-section ${classes['guestbook-section']}`}>
+                <h1>Guests</h1>
+                <ul className={classes['guestbook-list']}>
+                    {messages.map((message) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <li className={`guest ${classes.guest}`}>
 
-                                <ul className={classes['message-name']}>
-                                    <li>Name:</li>
-                                    <li><b>{message.name}</b></li>
-                                </ul>
-                                <ul className={classes['message-twitter']}>
-                                    <li>Twitter Handle: </li>
-                                    <li><b><a className={`social-guest-link ${classes['social-guest-link']}`} href={`${twitterUrl}${message.twitterHandle}`} target="_blank" rel="noopener noreferrer">{message.twitterHandle}</a></b></li>
-                                </ul>
-                                <ul className={classes['message-linkedin']}>
-                                    <li>Linkedin Handle:</li> <li><b><a className={`social-guest-link ${classes['social-guest-link']}`} href={`${linkedinUrl}${message.linkedinHandle}`}>{message.linkedinHandle}</a></b></li>
-                                </ul>
-                                <ul className={classes['message-github']}>
-                                    <li>Github Handle:</li>
-                                    <li><b><a className={`social-guest-link ${classes['social-guest-link']}`} href={`${githubUrl}${message.githubHandle}`}>{message.githubHandle}</a></b></li>
-                                </ul>
-                                <ul className={classes['message-message']}>
-                                    <li><b>Message:</b></li>
-                                    <li>{message.message}</li>
-                                </ul>
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            </>
-        )
-    }
-    return <p>Access denied.</p>
+                            <ul className={classes['message-name']}>
+                                <li>Name:</li>
+                                <li><b>{message.name}</b></li>
+                            </ul>
+                            <ul className={classes['message-twitter']}>
+                                <li>Twitter Handle: </li>
+                                <li><b><a className={`social-guest-link ${classes['social-guest-link']}`} href={`${twitterUrl}${message.twitterHandle}`} target="_blank" rel="noopener noreferrer">{message.twitterHandle}</a></b></li>
+                            </ul>
+                            <ul className={classes['message-linkedin']}>
+                                <li>Linkedin Handle:</li> <li><b><a className={`social-guest-link ${classes['social-guest-link']}`} href={`${linkedinUrl}${message.linkedinHandle}`}>{message.linkedinHandle}</a></b></li>
+                            </ul>
+                            <ul className={classes['message-github']}>
+                                <li>Github Handle:</li>
+                                <li><b><a className={`social-guest-link ${classes['social-guest-link']}`} href={`${githubUrl}${message.githubHandle}`}>{message.githubHandle}</a></b></li>
+                            </ul>
+                            <ul className={classes['message-message']}>
+                                <li><b>Message:</b></li>
+                                <li>{message.message}</li>
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
+    )
 }
 
 export async function getServerSideProps(context) {
+    const session = await getSession({ req: context.req })
+    console.log('session', session)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
     let messages = []
     const client = await connectToDB()
     const db = client.db()
@@ -65,9 +70,7 @@ export async function getServerSideProps(context) {
     })
     return {
         props: {
-            session: await getServerSession(
-                context.req,
-            ),
+            session,
             messages: parsedMessages
         }
     }
