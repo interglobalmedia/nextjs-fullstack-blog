ca1dac0dcfc6b4b4a0b00702f44ccc48789612a7
Maria Campbell <interglobalmedia@gmail.com>
2023-02-24 18:44:14 -0500

    NFB-59 #resolve #time 5m #comment add consolata and oswald

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import siteMetadata from '../data/siteMetadata'
 import { ThemeProvider } from 'next-themes'
 import { motion } from 'framer-motion'
-import { Inconsolata, Roboto, Oswald } from '@next/font/google'
+import { Inconsolata, Oswald } from '@next/font/google'
 import { useEffect, useState } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import PlausibleProvider from 'next-plausible'
@@ -12,16 +12,14 @@ import PlausibleProvider from 'next-plausible'
 const inconsolata = Inconsolata({
   weight: ['300', '400', '700'],
   subsets: ['latin'],
-})
-
-const roboto = Roboto({
-  weight: ['300', '400', '700'],
-  subsets: ['latin'],
+  display: 'optional',
 })
 
 const oswald = Oswald({
   weight: ['300', '400', '700'],
   subsets: ['latin'],
+  display: 'optional',
+  variable: '--oswald-font',
 })
 
 const variants = {
@@ -59,6 +57,11 @@ export default function App({ Component, pageProps, router }) {
               rel="icon"
               href="/favicon.ico"
             />
+            <style jsx global>{`
+        :root {
+          --oswald-font: ${oswald.style.fontFamily};
+        }
+      `}</style>
           </Head>
           <motion.div
             key={router.route}
@@ -69,7 +72,7 @@ export default function App({ Component, pageProps, router }) {
             transition={{ duration: 0.5, type: 'tween' }}
           >
             <PlausibleProvider domain="mariadcampbell.com">
-              <main className={`${inconsolata.className}`}>
+              <main className={`${inconsolata.className} ${oswald.variable}`}>
                 <Component {...pageProps} />
               </main>
             </PlausibleProvider>
