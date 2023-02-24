8b555f525edc5f380bbc9adf9c3bbc4a9a9908d7
Maria Campbell <interglobalmedia@gmail.com>
2023-02-23 20:52:10 -0500

    Add unauth for footer nav

--- components/layout/footer-navigation/footer-navigation.js
+++ components/layout/footer-navigation/footer-navigation.js
@@ -27,7 +27,7 @@ function FooterNavigation() {
         <header className={`footer-header ${classes['footer-header']}`}>
             <nav className={classes['footer-navbar']}>
                 <ul className={`footer-nav ${classes['footer-nav']}`}>
-                    {status === `authenticated` &&
+                    {status === `unauthenticated` &&
                         <li className='menu-link-li'>
                             <Link href='/contact' className={`${router.pathname} ===
                     '/contact' ? 'active' : ''} menu-link relative mr-6 flex flex-col font-medium text-gray-900 dark:text-gray-100 sm:mr-8 sm:p-4`}>Contact{isActiveLink(
@@ -45,7 +45,7 @@ function FooterNavigation() {
                                         ></motion.div>
                                     )}</Link>
                         </li>}
-                    {status === `authenticated` &&
+                    {status === `unauthenticated` &&
                         <li className='menu-link-li'>
                             <Link href='/guestbook' className={`${router.pathname} ===
                     '/guestbook' ? 'active' : ''} menu-link relative mr-6 flex flex-col font-medium text-gray-900 dark:text-gray-100 sm:mr-8 sm:p-4`}>Guestbook{isActiveLink(
