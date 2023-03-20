ea55906357cee023adaa7c839bdf8fbe2864a56e
Maria Campbell <interglobalmedia@gmail.com>
2023-03-19 20:04:07 -0400

    Remove extra meta

--- pages/_app.js
+++ pages/_app.js
@@ -49,16 +49,6 @@ export default function App({ Component, pageProps, router }) {
               name="viewport"
               content="width=device-width, initial-scale=1.0"
             />
-            <meta
-              name="author"
-              content="Maria D. Campbell"
-              key="author"
-            />
-            <meta
-              name="description"
-              content="I blog about fullstack development as well as macOS, Command Line, Git, and ethics in technology."
-              key="description"
-            />
             <link
               rel="icon"
               href="/favicon.ico"
