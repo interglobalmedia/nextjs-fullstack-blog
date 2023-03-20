92cc715356cf1c10ac863eb921bc02dc1d61c53f
Maria Campbell <interglobalmedia@gmail.com>
2023-03-19 22:40:15 -0400

    Add meta to doc

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      
+        <meta name="twitter:author" content="@letsbsocial1" />
+        <meta name="twitter:site" content="@letsbsocial1" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={`https://twitter.com/letsbsocial1`} />
+        <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/ images/site/drew-farwell-6pQiSb5qnEo-unsplash.jpg`} />
+        <meta property="og:title" content={`Maria D. Campbell`} />
+        <meta property="og:url" content={`https://www.mariadcampbell.com`} />
+        <meta property="og:type" content="website" />
+       
       </Head>
       <body>
         <div className="site-content">
