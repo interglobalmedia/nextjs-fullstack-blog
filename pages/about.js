ad95f0d27c1f2c890957a01242f5c5d3586e7cb2
Maria Campbell <interglobalmedia@gmail.com>
2024-06-26 12:06:19 -0400

    Update about page

--- pages/about.js
+++ pages/about.js
@@ -6,9 +6,10 @@ function AboutPage() {
         <section className={`about-section ${classes['about-section']}`}>
             <h1>About this Site</h1>
             <article className={`about-article ${classes['about-article']}`}>
-                <p className={classes.info}>If you would like to contact me, you can sign in with Github located in the footer of this site and fill out the <Link href='/contact'>contact form</Link> on the <Link href='/contact'>Contact page</Link> that appears upon sign in. Or you can email me directly by clicking on the envelope in the site footer.</p>
-                <p className={classes.info}>When you sign in and fill out the contact form, your message will appear in my <Link href='/guestbook'>Guestbook</Link> along with links to your Github, Twitter, and Linkedin handles. Looking forward to hearing from you!</p>
-                <p className={classes.info}>Spam will be removed.</p>
+                <p className={classes.info}>If you would like to contact me, you can email me by clicking on the envelope in the site footer.</p>
+                <p className={classes.info}>Looking forward to hearing from you!</p>
+                <p className={classes.info}>Spam will be automatically blocked and deleted.</p>
+                <p className={classes.info}>Available for new work opportunities. And be sure to check out my Skills page to learn more about what I have to offer.</p>
             </article>
         </section>
     )
