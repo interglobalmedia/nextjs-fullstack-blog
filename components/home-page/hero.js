9e8fd8fe473fe387bcd9292d47e7869617295fc7
Maria Campbell <interglobalmedia@gmail.com>
2024-07-06 06:17:48 -0400

    Add more intro content and modify eslint fix

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,24 +1,37 @@
-import Image from 'next/legacy/image';
+import Image from 'next/legacy/image'
 import classes from '../../styles/hero.module.scss'
 
 function Hero() {
-    return (
-        <section className={`hero ${classes.hero}`}>
-            <div className={classes.image}>
-                <Image
-                    src='/images/site/profile_image.jpg'
-                    alt='Maria Campbell profile image'
-                    width={300}
-                    height={300}
-                    layout="responsive"
-                />
-            </div>
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <h1>Hi, I'm <a href='https://www.interglobalmedianetwork.com/about' target='_blank'
-                rel='noopener noreferrer'>Maria</a>!</h1>
-            <p>I blog about cybersecurity, fullstack development as well as macOS, Command Line, Git, and ethics in technology.</p>
-        </section>
-    )
+	return (
+		<section className={`hero ${classes.hero}`}>
+			<div className={classes.image}>
+				<Image
+					src="/images/site/profile_image.jpg"
+					alt="Maria Campbell profile image"
+					width={300}
+					height={300}
+					layout="responsive"
+				/>
+			</div>
+			<h1>
+				{/*eslint-disable-next-line react/no-unescaped-entities */}
+				Hi, I'm{' '}
+				<a
+					href="https://www.interglobalmedianetwork.com/about"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Maria
+				</a>
+				!
+			</h1>
+			<p>
+				I blog about cybersecurity, fullstack development as well as
+				macOS, Command Line, shell scripting, Windows, Git, and ethics
+				in technology.
+			</p>
+		</section>
+	)
 }
 
 export default Hero
