baf573e4a7b9bac7e82e660153e8e8778287b276
Maria Campbell <interglobalmedia@gmail.com>
2024-07-08 11:35:24 -0400

    Add data-testid

--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -1,13 +1,16 @@
 import classes from '../../../styles/post-header.module.scss'
 
 function PostHeader(props) {
-    const { title } = props
-    
-    return (
-        <header className={`header ${classes.header}`}>
-            <h1>{title}</h1>
-        </header>
-    )
-}   
+	const { title } = props
+
+	return (
+		<header
+			data-testid="post-header"
+			className={`header ${classes.header}`}
+		>
+			<h1>{title}</h1>
+		</header>
+	)
+}
 
 export default PostHeader
