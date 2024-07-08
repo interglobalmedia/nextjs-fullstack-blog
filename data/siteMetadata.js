f735b04f7703683ddb97146ea24b9a2aeae32fcf
Maria Campbell <interglobalmedia@gmail.com>
2024-07-08 11:36:02 -0400

    Change email

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
 	theme: 'system', // system, dark or light
 	title: `Maria's Next Blog`,
 	author: 'Maria D. Campbell',
-	email: 'interglobalmedia@interglobalmedianetwork.com',
+	email: 'interglobalmedia@gmail.com',
 	github: 'https://github.com/interglobalmedia',
 	twitter: 'https://twitter.com/letsbsocial1',
 	linkedin: 'https://www.linkedin.com/in/mariacampbell/',
