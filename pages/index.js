c9478a29421a75f4da9b67789828c43787e7f6b3
Maria Campbell <interglobalmedia@gmail.com>
2024-07-13 07:54:47 -0400

    Add more keywords

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,39 @@ import dynamic from 'next/dynamic'
 import Hero from '../components/home-page/hero'
 import { getFeaturedPosts } from '../lib/posts-util'
 
-const DynamicFeaturedPosts = dynamic(() => import('../components/home-page/featured-posts'))
+const DynamicFeaturedPosts = dynamic(() =>
+	import('../components/home-page/featured-posts'),
+)
 
 function HomePage(props) {
-  return (
-    <Fragment>
-      <Head>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <title>Maria D. Campbell's Next Blog</title>
-        <meta
-          name='description'
-          content='I blog about cybersecurity, fullstack development as well as macOS, Command Line, Git, and ethics in technology.'
-        />
-        <meta name='keywords' content='command line, cryptography, css, cybersecurity, django 4, encryption, git, jira, javascript, html, linux, kali linux, mysql, nodejs, npm, python, sql, windows, wordpress' />
-      </Head>
-      <Hero />
-      <DynamicFeaturedPosts posts={props.posts} />
-    </Fragment>
-  )
+	return (
+		<Fragment>
+			<Head>
+				{/* eslint-disable-next-line react/no-unescaped-entities */}
+				<title>Maria D. Campbell's Next Blog</title>
+				<meta
+					name="description"
+					content="I blog about cybersecurity, fullstack development as well as macOS, Command Line, Git, and ethics in technology."
+				/>
+				<meta
+					name="keywords"
+					content="applescript, awk, chflags, schg, chmod, command line, command prompt, cryptography, css, cybersecurity, django 4, encryption, environment variables, file conversion, file permissions, git, git hooks, github, branch protection, grep, husky, jira, javascript, html, lint-staged, linux, kali linux, keyboard shortcuts, macos, mysql, nextjs, nodejs, npm, pipe, python, react, react portal, redirect operators, shell scripting, software updates, sql, ssh, stdin, stdout, stderr, unix, virtualbox, windows, windows 11, write protect files, zsh"
+				/>
+			</Head>
+			<Hero />
+			<DynamicFeaturedPosts posts={props.posts} />
+		</Fragment>
+	)
 }
 
 export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts()
+	const featuredPosts = getFeaturedPosts()
 
-  return {
-    props: {
-      posts: featuredPosts
-    }
-  }
+	return {
+		props: {
+			posts: featuredPosts,
+		},
+	}
 }
 
 export default HomePage
