e00a90b49f07dc66fc29792c63271111069f58f3
Maria Campbell <interglobalmedia@gmail.com>
2024-08-21 10:23:20 -0400

    Add re-generated svgs

--- components/social-icons/Tumblr.js
+++ components/social-icons/Tumblr.js
@@ -1,13 +1,13 @@
 import * as React from 'react'
 const SvgTumblr = (props) => (
 	<svg
-		width={70}
-		height={70}
-		viewBox='0 0 24 24'
-		xmlns='http://www.w3.org/2000/svg'
+		width={800}
+		height={800}
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
 		{...props}
 	>
-		<path d='M14.078 20.953c-2.692 0-4.699-1.385-4.699-4.7v-5.308H6.931V8.07c2.694-.699 3.821-3.017 3.95-5.023h2.796v4.558h3.263v3.34h-3.263v4.622c0 1.386.699 1.864 1.813 1.864h1.58v3.522h-2.992z' />
+		<path d="M14.078 20.953c-2.692 0-4.699-1.385-4.699-4.7v-5.308H6.931V8.07c2.694-.699 3.821-3.017 3.95-5.023h2.796v4.558h3.263v3.34h-3.263v4.622c0 1.386.699 1.864 1.813 1.864h1.58v3.522h-2.992z" />
 	</svg>
 )
 export default SvgTumblr
--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -1,3 +1,4 @@
+export { default as Dev } from './Dev'
 export { default as Email } from './Email'
 export { default as Github } from './Github'
 export { default as Linkedin } from './Linkedin'
