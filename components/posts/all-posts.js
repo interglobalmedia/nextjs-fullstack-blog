637aed6dcf8b6e97fde6ff7499c127e611fa77f0
Maria Campbell <interglobalmedia@gmail.com>
2024-09-05 07:43:06 -0400

    Add formatting

--- components/posts/all-posts.js
+++ components/posts/all-posts.js
@@ -1,25 +1,23 @@
 import dynamic from 'next/dynamic'
-import { useState } from 'react'
 import classes from '../../styles/all-posts.module.scss'
-
 
 const DynamicPostsGrid = dynamic(() => import('./posts-grid'))
 
 function AllPosts(props) {
-    const { posts, initialDisplayPosts, pagination } = props
-    
-    return (
-        <>
-        <section className={`${classes.posts}`}>
-            <h1>All Posts</h1>
-            <DynamicPostsGrid
-                    posts={posts}
-                    pagination={pagination}
-                    initialDisplayPosts={initialDisplayPosts}
-                />
-        </section>
-        </>
-        )
+	const { posts, initialDisplayPosts, pagination } = props
+
+	return (
+		<>
+			<section className={`${classes.posts}`}>
+				<h1>All Posts</h1>
+				<DynamicPostsGrid
+					posts={posts}
+					pagination={pagination}
+					initialDisplayPosts={initialDisplayPosts}
+				/>
+			</section>
+		</>
+	)
 }
 
 export default AllPosts
