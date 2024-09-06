30d51c23e17afac9686bfd7ec1b167816c0383ab
Maria Campbell <interglobalmedia@gmail.com>
2024-09-06 10:03:34 -0400

    Remove search related code & add getStaticProps

--- pages/projects.js
+++ pages/projects.js
@@ -1,80 +1,47 @@
-import { useState } from 'react'
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import siteMetadata from '../data/siteMetadata'
 import projectsData from '../data/projectsData'
-import Card from '../components/project/project-card'
 import ScrollTop from '../components/buttons/scroll-top'
+import { getAllProjects } from '../lib/projects-util'
 import ScrollStep from '../components/buttons/scroll-step'
-import classes from '../styles/all-projects.module.scss'
+import classes from '../styles/projects.module.scss'
 
-export default function Projects({ initialDisplayPosts = [] }) {
-	const [searchValue, setSearchValue] = useState('')
-	const filteredProjectPosts = projectsData.filter((project) => {
-		const searchContent =
-			project.title + project.summary + project.tags.join(' ')
-		return searchContent.toLowerCase().includes(searchValue.toLowerCase())
-	})
-	// If initialDisplayPosts exist, display it if no searchValue is specified
-	const displayProjectPosts =
-		initialDisplayPosts.length > 0 && !searchValue
-			? initialDisplayPosts
-			: filteredProjectPosts
+const DynamicAllProjects = dynamic(() =>
+	import('../components/project/all-projects'),
+)
+
+export const PROJECTS_PER_PAGE = 10
+
+export async function getStaticProps() {
+	const allProjectsData = JSON.parse(JSON.stringify(projectsData))
+	const initialDisplayProjects = allProjectsData.slice(0, PROJECTS_PER_PAGE)
+	const pagination = {
+		currentPage: 1,
+		totalPages: Math.ceil(allProjectsData.length / PROJECTS_PER_PAGE),
+	}
+	return { props: { initialDisplayProjects, allProjectsData, pagination } }
+}
+
+export default function Projects({
+	projects,
+	initialDisplayProjects,
+	pagination,
+}) {
 	return (
 		<>
 			<Head>
 				{/* eslint-disable-next-line react/no-unescaped-entities */}
-				<title>Maria D. Campbell's Next Blog Blog Page</title>
+				<title>Maria D. Campbell's Next Blog Projects Page</title>
 				<meta
 					name="description"
 					content="Welcome to my Projects page, which contains all my projects."
 				/>
 			</Head>
-			<main className={classes.main}>
-				<section className={classes['projects-wrapper']}>
-					<h1>All Projects</h1>
-					<section className={classes['section-search']}>
-						<input
-							aria-label="Search projects"
-							type="text"
-							onChange={(e) => setSearchValue(e.target.value)}
-							placeholder="Search projects"
-							className={`${classes.search} block w-full rounded-md bg-primary-500 border border-gray-300 px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100`}
-						/>
-						<svg
-							className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-							></path>
-						</svg>
-					</section>
-				</section>
-				<section className={classes['project-container']}>
-					<article className={classes['project-card']}>
-						{!filteredProjectPosts.length && 'No projects found.'}
-						{displayProjectPosts
-							.map((d) => (
-								<Card
-									key={d.id}
-									title={d.title}
-									summary={d.summary}
-									imgSrc={d.imgSrc}
-									href={d.href}
-									tags={d.tags}
-								/>
-							))
-							.sort()
-							.reverse()}
-					</article>
-				</section>
-			</main>
+			<DynamicAllProjects
+				projects={projects}
+				initialDisplayProjects={initialDisplayProjects}
+				pagination={pagination}
+			/>
 			<ScrollStep />
 			<ScrollTop />
 		</>
