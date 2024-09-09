8ec6ddeb6f7e0fc03c29153fb393186f67e094cc
Maria Campbell <interglobalmedia@gmail.com>
2024-09-09 14:25:08 -0400

    Add patreon svg

---
+++ components/social-icons/Patreon.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+const SvgPatreon = (props) => (
+	<svg
+		width={70}
+		height={70}
+		viewBox="-2 -2.5 24 24"
+		overflow="visible"
+		preserveAspectRatio="xMinYMin"
+		className="patreon_svg__jam patreon_svg__jam-patreon"
+		xmlns="http://www.w3.org/2000/svg"
+		{...props}
+	>
+		<g fill="#1D1D1B">
+			<path d="M12.808.01c-3.95 0-7.164 3.196-7.164 7.125 0 3.916 3.214 7.103 7.164 7.103 3.938 0 7.142-3.187 7.142-7.103 0-3.93-3.204-7.125-7.142-7.125M.05 18.99V.01h3.502v18.98z" />
+		</g>
+	</svg>
+)
+export default SvgPatreon
