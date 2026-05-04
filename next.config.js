61bab978a79f3de4830888c8c95fc75f77769e8f
Maria Campbell <interglobalmedia@gmail.com>
2026-05-03 20:13:07 -0400

    Breaking changes made upgrade from 14 to 16

--- next.config.js
+++ next.config.js
@@ -1,54 +1,29 @@
 const path = require('path')
-const webpack = require('webpack')
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
-
-	webpack: (config, { dev, isServer }) => {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
-		})
-
-		if (!dev && !isServer) {
-			// Replace React with Preact only in client production build
-			Object.assign(config.resolve.alias, {
-				'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-				react: 'preact/compat',
-				'react-dom/test-utils': 'preact/test-utils',
-				'react-dom': 'preact/compat',
-			})
-		}
-
-		if (isServer) {
-			require('./scripts/generate-sitemap')
-		}
-		if (!isServer) {
-			require('./scripts/compose')
-		}
-
-		config.plugins.push(
-			new webpack.EnvironmentPlugin(
-				'NEXT_PUBLIC_GISCUS_REPO',
-				'NEXT_PUBLIC_GISCUS_REPOSITORY_ID',
-				'NEXT_PUBLIC_GISCUS_CATEGORY',
-				'NEXT_PUBLIC_GISCUS_CATEGORY_ID',
-				'NEXT_PUBLIC_PLAUSIBLE_API_KEY',
-			),
-		)
-
-		return config
-	},
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	/* Configure pageExtensions to include md and mdx */
 	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-	/* Optionally, add any other Next.js config below */
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
+	},
+	turbopack: {
+		rules: {
+			'*.svg': {
+				loaders: ['@svgr/webpack'],
+				as: '*.js',
+			},
+		},
+
+		// resolveAlias: {
+		//     'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+		//     react: 'preact/compat',
+		//     'react-dom/test-utils': 'preact/test-utils',
+		//     'react-dom': 'preact/compat',
+		// },
 	},
 }
 
