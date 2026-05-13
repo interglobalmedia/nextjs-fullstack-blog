a1e87334e7d8ebb6ae014a043a37a626b00383c5
Maria Campbell <interglobalmedia@gmail.com>
2026-05-13 13:10:34 -0400

    Remove children from props

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Oswald } from 'next/font/google'
@@ -5,50 +6,62 @@ import { getTagLink } from './get-tag-link'
 import classes from '../../styles/post-item.module.scss'
 
 const oswald = Oswald({
-  weight: ['400'],
-  subsets: ['latin'],
-    display: 'optional',
-    variable: '--oswald-font',
+	weight: ['400'],
+	subsets: ['latin'],
+	display: 'optional',
+	variable: '--oswald-font',
 })
 
 function PostItem(props) {
+	const { post } = props
 
-    const { post } = props
+	const options = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		weekday: 'long',
+	}
 
-    const options = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        weekday: 'long'
-    };
+	const formattedDate = new Date(post.date).toLocaleDateString(
+		'en-US',
+		options,
+	)
 
-    const formattedDate = new Date(post.date).toLocaleDateString('en-US', options)
+	const imagePath = `/images/blog/${post.slug}/${post.image}`
 
-    const imagePath = `/images/blog/${post.slug}/${post.image}`
-
-    const linkPath = `/blog/${post.slug}`
-    return (
-        <li className={`post ${classes.post}`}>
-            <Link href={linkPath}>
-                <figure className={classes.image}>
-                    <Image
-                        src={imagePath}
-                        alt={post.title}
-                        width={300}
-                        height={200}
-                        className={classes.img}
-                        />
-                    <figcaption className={`${classes.content} ${oswald.variable}`}>
-                        <h3>{post.title}</h3>
-                        <time className={`time`}>{formattedDate}</time>
-                        <p>{post.author}</p>
-                        <p>{post.excerpt}</p>
-                        <p>{post.tags.map(tag => getTagLink(tag)).reduce((prev, curr) => [prev, ', ', curr])}</p>
-                    </figcaption>
-                </figure>
-            </Link>
-        </li>
-    );
+	const linkPath = `/blog/${post.slug}`
+	return (
+		<li className={`post ${classes.post}`}>
+			<Link href={linkPath}>
+				<figure className={classes.image}>
+					<Image
+						src={imagePath}
+						alt={post.title}
+						width={300}
+						height={200}
+						className={classes.img}
+					/>
+					<figcaption
+						className={`${classes.content} ${oswald.variable}`}
+					>
+						<h3>{post.title}</h3>
+						<time className={`time`}>{formattedDate}</time>
+						<p>{post.author}</p>
+						<p>{post.excerpt}</p>
+						<p>
+							{' '}
+							{post.tags.map((tag, index) => (
+								<Fragment key={tag}>
+									{getTagLink(tag)}
+									{index < post.tags.length - 1 && ', '}
+								</Fragment>
+							))}
+						</p>
+					</figcaption>
+				</figure>
+			</Link>
+		</li>
+	)
 }
 
 export default PostItem
