1a59db8671c509265f85d4edac26b4ab2bac430f
Maria Campbell <interglobalmedia@gmail.com>
2026-05-19 14:48:41 -0400

    Add prettier formatting

--- components/posts/get-tag-link.js
+++ components/posts/get-tag-link.js
@@ -2,7 +2,9 @@ import Link from 'next/link'
 import classes from '../../styles/post-item.module.scss'
 
 export const getTagLink = (tag) => {
-    return (
-        <Link className={`${classes.tag} tag`} href={`/blog/tag/${tag}`}>#{tag}</Link>
-    )
+	return (
+		<Link className={`${classes.tag} tag`} href={`/blog/tag/${tag}`}>
+			#{tag}
+		</Link>
+	)
 }
