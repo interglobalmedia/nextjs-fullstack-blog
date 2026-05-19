0d58f901eeec8ec00d6a654f485edb193fef53c1
Maria Campbell <interglobalmedia@gmail.com>
2026-05-19 14:47:22 -0400

    Add prettier formatting

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -2,19 +2,14 @@ import { Oswald } from 'next/font/google'
 import classes from '../../styles/logo.module.scss'
 
 const oswald = Oswald({
-    weight: ['400'],
-    subsets: ['latin'],
-    display: 'optional',
-    variable: '--oswald-font',
+	weight: ['400'],
+	subsets: ['latin'],
+	display: 'optional',
+	variable: '--oswald-font',
 })
 
 function Logo() {
-    return (
-        // eslint-disable-next-line react/no-unescaped-entities
-        <div className={`${classes.logo} ${oswald.variable}`}>
-            MDC
-        </div>
-    )
+	return <div className={`${classes.logo} ${oswald.variable}`}>MDC</div>
 }
 
 export default Logo
