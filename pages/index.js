fe0bd11a418d7ee4346ead0da247680a39873384
Maria Campbell <interglobalmedia@gmail.com>
2026-06-14 13:49:53 -0400

    Migrate static pages to custom Head component

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
 import { Fragment } from 'react'
-import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import Hero from '../components/home-page/hero'
 import { getFeaturedPosts } from '../lib/posts-util'
 import ScrollStep from '../components/buttons/scroll-step'
 import ScrollTop from '../components/buttons/scroll-top'
+import Head from '../components/seo/head'
+import siteMetadata from '../data/siteMetadata'
 
 const DynamicFeaturedPosts = dynamic(
 	() => import('../components/home-page/featured-posts'),
 )
 
+const DEFAULT_IMAGE = `${siteMetadata.siteUrl}${siteMetadata.defaultImage}`
+
 function HomePage(props) {
+	const url = siteMetadata.siteUrl
+
 	return (
 		<Fragment>
-			<Head>
-				{/* eslint-disable-next-line react/no-unescaped-entities */}
-				<title>Maria D. Campbell's Next Blog Home Page</title>
-				<meta
-					name="description"
-					content="Welcome to my Home page, which contains featured blog posts. I blog about ai, cybersecurity, fullstack development as well as macOS, Command Line, Linux, shell scripting, Windows, Git, and ethics
-				in technology"
-				/>
-			</Head>
+			<Head
+				title="Home"
+				excerpt="Security, fullstack development, macOS, command line, Linux, shell scripting, Windows, Git, and ethics in technology."
+				url={url}
+				author={siteMetadata.author}
+				image={DEFAULT_IMAGE}
+				imageAlt="Maria D. Campbell — Security, Fullstack Development, macOS, Linux"
+				type="website"
+			/>
 			<Hero />
 			<DynamicFeaturedPosts posts={props.posts} />
 			<ScrollStep />
